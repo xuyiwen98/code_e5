2671417555156816c178464314e9ac51bb5ff01e
xuyiwen <190848486@qq.com>
2023-05-09 06:35:49 +0800

    建立项目

---
+++ src/main/java/com/xyw/StayActiveApplication.java
@@ -0,0 +1,13 @@
+package com.xyw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayActiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayActiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xyw/StayActiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayActiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
